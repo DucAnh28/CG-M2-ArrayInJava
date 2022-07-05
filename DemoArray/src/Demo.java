e6ea701b0f58cac5bb5d76f54d42275220f608cd
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-07-05 10:41:51 +0700

    Add practice ArrayReverse

--- DemoArray/src/Demo.java
+++ DemoArray/src/Demo.java
@@ -1,16 +1,55 @@
 import java.util.Scanner;
+
 public class Demo {
-//    Tạo mảng cho ng dùng nhập vào
     public static void main(String[] args) {
-        int [][] matrix = new int[3][3];
-        java.util.Scanner input = new Scanner(System.in);
-        System.out.println("Enter " + matrix.length + " rows and " +
-                matrix[0].length + " columns: ");
-        for (int row = 0; row < matrix.length; row++) {
-            for (int column = 0; column < matrix[row].length; column++) {
-                matrix[row][column] = input.nextInt();
-            }
-        }
-//        Tạo mảng random
+        System.out.println(args[0]);
+        Scanner scanner = new Scanner(System.in);
+    }
+    //phương thức trừa tường: từ khóa abstract
+//    abstract void m1();
+    public int a;
+
+    //constructor: hàm khởi tạo
+    //có tên trùng với tên lớp, không chứa return datatype hoặc void
+    //mặc định 1 lớp sẽ chứa 1 constructor không tham số nếu không có constructor nào được tạo
+    public Demo() {
+
+    }
+
+    public Demo(int a) {
+        this.a = a;
+    }
+
+    //phương thức bình thường: từ khóa void
+    void m2(){}
+
+    //phương thức bình thường: void + access modifier
+    public void m3(){
+    }
+    protected void m4(){}
+    private void m5(){}
+
+    //phương thức bình thường: return datatypes
+    int m6(){return 0;}
+
+    //phương thức bình thường: return datatypes + access modifier
+    public int m7(){return 100;}
+    protected int m8(){return 0;}
+    private int m9(){return 0;}
+
+    //kết hợp 1 số từ khóa khác: final, static
+    public final void m10(){}
+    public static void m11(){}
+    private static final void m12(){}
+
+    //tham trị và tham chiếu
+    public int m13(int a) {
+        a = 100;
+        return a;
+    }
+
+    public int m14(int[] a) {
+        a[0] = 100;
+        return a[0];
     }
 }
